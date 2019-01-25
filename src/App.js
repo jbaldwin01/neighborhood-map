0de01b25702b21164eb2e2ae375021b941e40a0a
Jeffrey Baldwin <jbaldwin01@snet.net>
2019-01-25 16:50:32 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.renderMap()
+    this.getVenues()
+  }
+
+  getVenues() {
+    fetch(`https://api.foursquare.com/v2/venues/explore?v=20180323&client_id=FDAMFVN2CLVPRFZSHTJCWLYP34V0FZ1U2N0EU3TO4QUDMBHZ&client_secret=1I1TBKGC1CI5Z5LMTCJJRUCFLK5AWPANS4QHHEGMDQXEMJWY&near=Hartford`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    // .then(data => console.log(data.response.groups[0].items.map(item => item.venue.name)))
+    .catch(error => {console.log(`ERROR: ${error}`)})
+  }
+
+  /*
+   * Display the map on the page
+   */
+  renderMap() {
+    loadMapAPI("https://maps.googleapis.com/maps/api/js?key=AIzaSyDs8apc-US8cnhHdTl8djgb6tPxXwfGjzg&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  initMap() {
+  // initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 41.7658, lng: -72.6734},
+      zoom: 15
+    });
+    // map.setZoom(5)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main className="app">
+        <div id="map"></div>
+      </main>
     );
   }
+}
+
+/*
+ * Create the script element for the map API
+ * i.e. <script src="url" async defer></script>
+ */
+function loadMapAPI(url) {
+  const index = window.document.getElementsByTagName("script")[0]
+  const script = window.document.createElement("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
 }
 
 export default App;
