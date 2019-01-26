cd1fc0c90b3535a2a6526e9d844168cab539e9f7
Jeffrey Baldwin <jbaldwin01@snet.net>
2019-01-26 12:06:56 -0500

    Display locations on map with info windows

--- src/App.js
+++ src/App.js
@@ -3,16 +3,35 @@ import './App.css';
 
 class App extends Component {
 
+  state = {
+    venues: []
+  }
+
   componentDidMount() {
-    this.renderMap()
     this.getVenues()
   }
 
+  /*
+   * Async request to retrieve venues dynamically.
+   * Set renderMap() as callback function of setState() to 
+   * ensure renderMap() gets called after async call to 
+   * getVenues() has completed and vanues have been stored in state.
+   */
   getVenues() {
-    fetch(`https://api.foursquare.com/v2/venues/explore?v=20180323&client_id=FDAMFVN2CLVPRFZSHTJCWLYP34V0FZ1U2N0EU3TO4QUDMBHZ&client_secret=1I1TBKGC1CI5Z5LMTCJJRUCFLK5AWPANS4QHHEGMDQXEMJWY&near=Hartford`)
+    const endpoint = "https://api.foursquare.com/v2/venues/explore?"
+    const client_id = "FDAMFVN2CLVPRFZSHTJCWLYP34V0FZ1U2N0EU3TO4QUDMBHZ"
+    const client_secret = "1I1TBKGC1CI5Z5LMTCJJRUCFLK5AWPANS4QHHEGMDQXEMJWY"
+    let near = "Hartford"
+    let query = "restaurants"
+    fetch(`${endpoint}v=20180323&client_id=${client_id}&client_secret=${client_secret}&near=${near}&query=${query}`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    // .then(data => console.log(data))
     // .then(data => console.log(data.response.groups[0].items.map(item => item.venue.name)))
+    .then(data => {
+      this.setState({
+        venues: data.response.groups[0].items
+      },this.renderMap()) // set renderMap() as callback function
+    })
     .catch(error => {console.log(`ERROR: ${error}`)})
   }
 
@@ -24,13 +43,27 @@ class App extends Component {
     window.initMap = this.initMap
   }
 
-  initMap() {
-  // initMap = () => {
+  initMap = () => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 41.7658, lng: -72.6734},
       zoom: 15
-    });
-    // map.setZoom(5)
+    })
+
+    this.state.venues.map(mapVenue => {
+      let contentString = `${mapVenue.venue.name}<p/>${(mapVenue.venue.location.address) || 'Address not available'}`
+      let infoWindow = new window.google.maps.InfoWindow({
+        content: contentString
+      })
+      
+      let marker = new window.google.maps.Marker({
+        position: {lat: mapVenue.venue.location.lat, lng: mapVenue.venue.location.lng},
+        title: mapVenue.venue.name,
+        map: map
+      })
+
+      marker.addListener('click', () => infoWindow.open(map, marker))
+      return marker
+    })
   }
 
   render() {
