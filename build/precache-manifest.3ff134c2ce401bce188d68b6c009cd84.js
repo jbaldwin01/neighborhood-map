1b92b015d7295a8fd2e275ad690b62b2e387612f
Jeffrey Baldwin <jbaldwin01@snet.net>
2019-02-05 16:40:03 -0500

    Add build assets to repository

---
+++ build/precache-manifest.3ff134c2ce401bce188d68b6c009cd84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f45b0a12aaeb923b30eb098d8377ac11",
+    "url": "/static/media/powered-by-foursquare-blue.f45b0a12.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "31c1a00c3963e161e951",
+    "url": "/static/js/main.31c1a00c.chunk.js"
+  },
+  {
+    "revision": "6a8b514670f17a4ecb21",
+    "url": "/static/js/1.6a8b5146.chunk.js"
+  },
+  {
+    "revision": "31c1a00c3963e161e951",
+    "url": "/static/css/main.9c59a39a.chunk.css"
+  },
+  {
+    "revision": "6e959a1865a19fbc068f4c589d1c0190",
+    "url": "/index.html"
+  }
+];
